df00ac45523dc011d52d7c84168ccc6cf431c1ab
grabbus <41126964+grabbus@users.noreply.github.com>
2021-10-15 09:43:43 +0200

    Added serviceworker

---
+++ serviceworker.js
@@ -0,0 +1,26 @@
+const cache = 'pwa-krebse';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/form.html',
+    'list.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
+/* Start the service worker and cache the content */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cache).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return reponse || fetch(e.request);
+        })
+    )
+});

fd9c8b5d278e45a4f268e640417e3034a2d0da1a
grabbus <41126964+grabbus@users.noreply.github.com>
2021-10-15 10:40:58 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -9,6 +9,10 @@ var filesToCache = [
     '/css/style.css',
     '/js/main.js'
 ];
+var CACHE = {
+	name: 'Application Cache',
+	version: '1'
+};
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
@@ -48,104 +52,243 @@ self.addEventListener('install', function(e) {
 		);
 	}
 })
-
-/**
- * Serializes a Response into a plain JS object
- * 
- * @param response
- * @returns Promise
- */ 
- function serializeResponse(response) {
-    var serialized = {
-      headers: serializeHeaders(response.headers),
-      status: response.status,
-      statusText: response.statusText
-    };
-  
-    return response.clone().text().then(function(body) {
-        serialized.body = body;
-        return Promise.resolve(serialized);
-    });
-}
-
-/**
- * Serializes headers into a plain JS object
- * 
- * @param headers
- * @returns object
- */ 
- function serializeHeaders(headers) {
-	var serialized = {};
-	// `for(... of ...)` is ES6 notation but current browsers supporting SW, support this
-	// notation as well and this is the only way of retrieving all the headers.
-	for (var entry of headers.entries()) {
-		serialized[entry[0]] = entry[1];
-	}
-	return serialized;
-}
-
-/**
- * Creates a Response from it's serialized version
- * 
- * @param data
- * @returns Promise
- */ 
- function deserializeResponse(data) {
-	return Promise.resolve(new Response(data.body, data));
-}
- 
-/**
- * Saves the response for the given request eventually overriding the previous version
- * 
- * @param data
- * @returns Promise
- */
-function cachePut(request, response, store) {
-	var key, data;
-	getPostId(request.clone())
-	.then(function(id){
-		key = id;
-		return serializeResponse(response.clone());
-	}).then(function(serializedResponse) {
-		data = serializedResponse;
-		var entry = {
-			key: key,
-			response: data,
-			timestamp: Date.now()
-		};
-		store
-		.add(entry)
-		.catch(function(error){
-			store.update(entry.key, entry);
-		});
-	});
-}
-	
-/**
- * Returns the cached response for the given request or an empty 503-response  for a cache miss.
- * 
- * @param request
- * @return Promise
- */
-function cacheMatch(request) {
-	return getPostId(request.clone())
-	.then(function(id) {
-		return store.get(id);
-	}).then(function(data){
-		if (data) {
-			return deserializeResponse(data.response);
-		} else {
-			return new Response('', {status: 503, statusText: 'Service Unavailable'});
+self.addEventListener('fetch', async function (event) {
+	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
+		return;
+	if (event.request.method === 'GET') {
+		if (event.request.url.includes('/static/') || event.request.mode !== 'cors') {
+			event.respondWith(fetchResponseFromCache(event.request))
+			return
 		}
-	});
+		if (navigator.onLine) {
+			event.respondWith(cacheRequest(event.request));
+		} else {
+			var resp = await fetchResponseFromCache(event.request).catch((e) => { return })
+			if (resp){
+				event.respondWith(resp)
+			}
+		}
+	}
+	else {
+		if(!navigator.onLine){
+			//here you can check for specific urls to be saved in indexed db
+			var authHeader = event.request.headers.get('Authorization');
+			var reqUrl = event.request.url;
+			Promise.resolve(event.request.text()).then((payload) => {
+				//save offline requests to indexed db
+				saveIntoIndexedDb(reqUrl, authHeader, payload)
+			})
+		}
+		
+	}
+});
+
+// Activate service worker
+self.addEventListener('activate', (event) => {
+	console.info('Event: Activate');
+	event.waitUntil(
+		self.clients.claim(),
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cache) => {
+					if (cache !== CACHE.name + CACHE.version) {
+						//delete all old caches or else new version of service worker won't get installed
+						return caches.delete(cache);
+					}
+				})
+			);
+		})
+	);
+});
+
+function checkNetworkState() {
+	setInterval(function () {
+		if (navigator.onLine) {
+			sendOfflinePostRequestsToServer()
+		}
+	}, 3000);
 }
- 
-/**
- * Returns a string identifier for our POST request.
- * 
- * @param request
- * @return string
- */
-function getPostId(request) {
-	return JSON.stringify(serializeRequest(request.clone()));
+
+function precacheAssets() {
+	//this files contains all static resources of a react application
+	var assets = '/application/resources/asset-manifest.json';
+	fetch(assets, {
+		method: "get",
+	})
+		.then((response) => {
+			let promise = Promise.resolve(response.text()).then((text) => {
+				jsonResources = JSON.parse(text)
+				var ary = []
+				Object.keys(jsonResources).forEach(function (key) {
+					if (!jsonResources[key].endsWith('.map'))
+						ary.push(jsonResources[key]);
+				});
+
+				caches.open(CACHE.name + CACHE.version).then(function (cache) {
+					return cache.addAll(
+						ary.map(url => new Request(url, {
+							credentials: 'same-origin'
+						}))
+					);
+				})
+			})
+		});
+};
+
+async function cacheResponse(cache, request, response, data) {
+	var responseToCache;
+	try {
+		if (!request.url.includes('/static/') && request.mode === 'cors') {
+
+			var responseData = await getResponseData(data)
+
+			responseToCache = new Response(btoa(responseData), {
+				headers: response.clone().headers
+			})
+		} else {
+			responseToCache = response.clone()
+		}
+		cache.put(request, responseToCache);
+	} catch (err) {
+	}
+	return response;
+}
+
+
+const cacheRequest = request => caches.open(CACHE.name + CACHE.version).then(cache =>
+
+	fetch(request.clone(), {
+		credentials: 'same-origin'
+	})
+		.then(response =>
+			cacheResponse(cache, request.clone(), response, response.clone().text()))
+);
+
+const fetchResponseFromCache = (request, returnResponseData) =>
+	caches.open(CACHE.name + CACHE.version).then(cache =>
+		cache.match(request, { ignoreVary: true }).then(response => returnResponseFromCache(request, response, returnResponseData, cache))
+	);
+
+async function returnResponseFromCache(request, response, returnResponseData, cache) {
+
+	if (response && !request.url.includes('/static/') && request.mode === 'cors') {
+		var responseData = await getResponseData(response.text())
+		if (returnResponseData)
+			return responseData
+		response = new Response(atob(responseData), {
+			headers: response.headers
+		})
+	}
+
+	if (!!response) {
+		return response;
+	} else {
+		console.log(request.url + ' not yet cached!')
+		return fetch(request, { credentials: 'same-origin' }).then(response => cacheResponse(cache, request, response))
+	}
+}
+
+async function getResponseData(data) {
+	let promise = Promise.resolve(data).then((text) => {
+		return text
+	})
+	let result = await promise;
+	return result
+}
+
+async function sendOfflinePostRequestsToServer() {
+	var request = indexedDB.open("TrayTrackingPostDB");
+	request.onsuccess = function (event) {
+		var db = event.target.result;
+		var tx = db.transaction('postrequest', 'readwrite');
+		var store = tx.objectStore('postrequest');
+		var allRecords = store.getAll();
+		allRecords.onsuccess = function () {
+
+			if (allRecords.result && allRecords.result.length > 0) {
+
+				var records = allRecords.result
+				//make recursive call to hit fetch requests to server in a serial manner
+				var resp = sendFetchRequestsToServer(
+					fetch(records[0].url, {
+						method: "post",
+						headers: {
+							'Accept': 'application/json',
+							'Content-Type': 'application/json',
+							'Authorization': records[0].authHeader
+						},
+						body: records[0].payload
+					}), records[0].url, records[0].authHeader, records[0].payload, records.slice(1))
+
+				for (var i = 0; i < allRecords.result.length; i++)
+					store.delete(allRecords.result[i].id)
+			}
+		};
+	}
+	request.onupgradeneeded = function (event) {
+		var db = event.target.result;
+		db.onerror = function (event) {
+			console.log("Why didn't you allow my web app to use IndexedDB?!");
+		};
+
+		var objectStore;
+		if (!db.objectStoreNames.contains('postrequest')) {
+			objectStore = db.createObjectStore("postrequest", { keyPath: 'id', autoIncrement: true });
+		}
+		else {
+			objectStore = db.objectStoreNames.get('postrequest');
+		}
+	}
+}
+
+function saveIntoIndexedDb(url, authHeader, payload) {
+	var myRequest = {};
+	jsonPayLoad = JSON.parse(payload)
+	//add payload if required. If not skip parsing json and stringifying it again
+	//jsonPayLoad['eventTime'] = getCurrentTimeString(eventTime)
+	myRequest.url = url;
+	myRequest.authHeader = authHeader;
+	myRequest.payload = JSON.stringify(jsonPayLoad);
+	var request = indexedDB.open("TrayTrackingPostDB");
+	request.onsuccess = function (event) {
+		var db = event.target.result;
+		var tx = db.transaction('postrequest', 'readwrite');
+		var store = tx.objectStore('postrequest');
+		store.add(myRequest)
+	}
+}
+
+async function sendFetchRequestsToServer(data, reqUrl, authHeader, payload, records) {
+
+	let promise = Promise.resolve(data).then((response) => {
+
+		console.log('Successfully sent request to server')
+		if (records.length != 0) {
+
+			sendFetchRequestsToServer(
+				fetch(records[0].url, {
+					method: "post",
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json',
+						'Authorization': records[0].authHeader
+					},
+					body: records[0].payload
+				}), records[0].url, records[0].authHeader, records[0].payload, records.slice(1))
+		}
+		return true
+	}).catch((e) => {
+		//fetch fails only in case of network error. Fetch is successful in case of any response code
+		console.log('Exception while sending post request to server' + e)
+		saveIntoIndexedDb(reqUrl, authHeader, payload)
+	})
+}
+
+async function updateCacheForAParticularRequest(authHeader) {
+	var myRequest = new Request('request url whose cache needs to be updated');
+	myRequest.mode = 'cors'
+	myRequest.headers = { 'Authorization': authHeader }
+	var resp = await fetchResponseFromCache(myRequest, true)
+	//make updations to resp and update cache
+	caches.open(CACHE.name + CACHE.version).then(cache => cache.put(myRequest, new Response(btoa("Updated Response"))));
 }

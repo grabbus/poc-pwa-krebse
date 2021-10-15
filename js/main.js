db108bee49498dd5929b5554b8e9449447cf92b7
grabbus <41126964+grabbus@users.noreply.github.com>
2021-10-15 10:00:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 window.onload = () => {
     'use strict';
   
-    if ('serviceworker' in navigator) {
-      navigator.serviceworker
-               .register('./serviceworker.js');
-    }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./serviceworker.js', { scope: '/sw-test/' }).then(function(reg) {
+          // Registrierung erfolgreich
+          console.log('Registrierung erfolgreich. Scope ist ' + reg.scope);
+        }).catch(function(error) {
+          // Registrierung fehlgeschlagen
+          console.log('Registrierung fehlgeschlagen mit ' + error);
+        });
+      };
   }

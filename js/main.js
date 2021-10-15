2df4ac81dea9ce1c72fdc451c27de4d6d45e877e
grabbus <41126964+grabbus@users.noreply.github.com>
2021-10-15 09:54:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = () => {
     'use strict';
   
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
+    if ('serviceworker' in navigator) {
+      navigator.serviceworker
                .register('./serviceworker.js');
     }
   }
